928432ccdeb413ff2fd757efd05ce7fe3e7e43d2
workonfire <kolucki62@gmail.com>
2021-03-14 00:22:26 +0100

    Small lombok cosmetic change

--- src/main/java/pl/workonfire/bucik/generators/data/GeneratorLocation.java
+++ src/main/java/pl/workonfire/bucik/generators/data/GeneratorLocation.java
@@ -1,14 +1,17 @@
 package pl.workonfire.bucik.generators.data;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import lombok.experimental.FieldDefaults;
 
 import java.io.Serializable;
 
 @EqualsAndHashCode
 @AllArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class GeneratorLocation implements Serializable {
-    private final int X, Y, Z;
-    private final String worldName;
-    private static final long serialVersionUID = 5266122845329240681L;
+    static long   serialVersionUID = 5266122845329240681L;
+           int    X, Y, Z;
+           String worldName;
 }
